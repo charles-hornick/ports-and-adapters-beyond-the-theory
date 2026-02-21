4c7ef477182acbc3613e1f1c094220aa39d320db
horcha <me@charles-hornick.be>
2026-02-21 11:04:05 +0100

    defining project maven's organisation

---
+++ inmemory-storage/src/main/java/be/charleshornick/inmemorystorage/InmemoryStorageApplication.java
@@ -0,0 +1,13 @@
+package be.charleshornick.inmemorystorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmemoryStorageApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(InmemoryStorageApplication.class, args);
+    }
+
+}

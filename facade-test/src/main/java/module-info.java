acd1555f322ff42c34664e3d6fa34ae05a82a956
horcha <me@charles-hornick.be>
2026-03-25 09:57:54 +0100

    creation of all modules needed to demonstrate JPMS

---
+++ facade-test/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module be.charleshornick.supra.facade.test {
+    requires be.charleshornick.supra;
+    requires org.slf4j;
+    requires core;
+}
--- supra/src/main/java/module-info.java
+++ supra/src/main/java/module-info.java
@@ -1,18 +1,17 @@
 module be.charleshornick.supra {
-    exports be.charleshornick.supra;                    // ErrorCause, ForStoringSnapshot
-    exports be.charleshornick.supra.create;
-    exports be.charleshornick.supra.define;
-    exports be.charleshornick.supra.define.race;
-    exports be.charleshornick.supra.define.profession;
-    exports be.charleshornick.supra.define.characteristic;
-    exports be.charleshornick.supra.retrieve.race;
-    exports be.charleshornick.supra.retrieve.profession;
-    exports be.charleshornick.supra.retrieve.snapshot;
-    exports be.charleshornick.supra.race;
-    exports be.charleshornick.supra.profession;
-    exports be.charleshornick.supra.characteristic;
-    exports be.charleshornick.supra.state.snapshot;
+    requires core; // Pragmatica-core
 
-    requires org.apache.commons.lang3;
-    requires core;
+    exports be.charleshornick.supra; // ErrorCause, ForStoringSnapshot
+    exports be.charleshornick.supra.create; // Primary port
+    exports be.charleshornick.supra.define; // ForLoadingSnapshot, ToCharacter
+    exports be.charleshornick.supra.define.race; // Primary port
+    exports be.charleshornick.supra.define.profession; // Primary port
+    exports be.charleshornick.supra.define.characteristic; // Primary port
+    exports be.charleshornick.supra.retrieve.race; // Primary port
+    exports be.charleshornick.supra.retrieve.profession; // Primary port
+    exports be.charleshornick.supra.retrieve.snapshot; // Primary port
+    exports be.charleshornick.supra.race; // Vocabulary
+    exports be.charleshornick.supra.profession; // Vocabulary
+    exports be.charleshornick.supra.characteristic; // Vocabulary
+    exports be.charleshornick.supra.state.snapshot; // Exposed state
 }
